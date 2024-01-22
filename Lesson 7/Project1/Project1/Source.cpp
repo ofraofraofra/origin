cae6a8b0ef6a66ae1af1f9f1279e9597ac5791e1
Ildar <ofraofraofra@mail.ru>
2024-01-22 16:36:23 +0400

    Lesson 7. Change 1

--- Lesson 7/Project1/Project1/Source.cpp
+++ Lesson 7/Project1/Project1/Source.cpp
@@ -6,13 +6,14 @@ int function(std::string str, int forbidden_length) {
         return str.length();
     }
     else {
-           throw std::string{ "bad_length" };
+        throw std::runtime_error("bad_length");
     }
 }
 
 int main() {
     setlocale(LC_ALL, "ru");
     system("chcp 1251");
+
     int forbidden_length;
     std::cout << "Введите запретную длину: ";
     std::cin >> forbidden_length;
@@ -26,7 +27,7 @@ int main() {
             int length = function(userInput, forbidden_length);
             std::cout << "Длина слова \"" << userInput << "\" равна " << length << std::endl;
         }
-        catch (const std::string& s) {
+        catch (const std::runtime_error& e) {
             std::cout << "Вы ввели слово запретной длины! До свидания" << std::endl;
             break;
         }
